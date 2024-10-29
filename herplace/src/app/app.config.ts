bbe033be93440ec72b8f590058fed7650d89f0fa
rafaelcerq28 <rafaelcerq28@gmail.com>
2024-10-29 20:59:22 +0000

    user page

--- herplace/src/app/app.config.ts
+++ herplace/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
-//importar para funcionar as requests
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),provideHttpClient(withFetch())]
+  //Using withInMemoryScrolling to return to the top of the page
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withInMemoryScrolling({
+    scrollPositionRestoration: 'enabled',
+  })), provideClientHydration()]
 };
--- herplace/src/app/app.routes.ts
+++ herplace/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { PlaceComponent } from './components/place/place.component';
 import { SearchComponent } from './components/search/search.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserpageComponent } from './components/userpage/userpage.component';
 
 export const routes: Routes = [
     {path: '',component: HomeComponent},
     {path: 'place', component: PlaceComponent},
     {path: 'search', component: SearchComponent},
-    {path: 'login', component: LoginComponent}
-
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'user', component: UserpageComponent}
+    
 ];
