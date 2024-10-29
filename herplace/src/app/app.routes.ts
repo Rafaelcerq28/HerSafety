112146b095a13a6a545e4dd489c89cb91bd3ecce
rafaelcerq28 <rafaelcerq28@gmail.com>
2024-10-29 14:43:24 +0000

    login page

--- herplace/src/app/app.routes.ts
+++ herplace/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PlaceComponent } from './components/place/place.component';
 import { SearchComponent } from './components/search/search.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: '',component: HomeComponent},
     {path: 'place', component: PlaceComponent},
-    {path: 'search', component: SearchComponent}
+    {path: 'search', component: SearchComponent},
+    {path: 'login', component: LoginComponent}
+
 ];
